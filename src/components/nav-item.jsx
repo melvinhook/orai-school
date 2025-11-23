ceacb7adbba9da41634dfc7a17ff267375ea709c
scav <aloysiusvictor14@gmail.com>
2025-11-23 17:49:13 +0700

    Fixed route on top bar

--- src/components/nav-item.jsx
+++ src/components/nav-item.jsx
@@ -1,20 +1,22 @@
 import { useSpring, animated } from "@react-spring/web"
-import { useEffect, useState } from "react" 
+import { useEffect, useState } from "react"  
+import { useNavigate } from "react-router-dom"
 import { pageController } from "../store/pageController"
-export default function Navitem({ children, hovered, sethovered, slug }) {  
+export default function Navitem({ children, hovered, sethovered, slug }) {   
+    const navigate=useNavigate()
     const {selected} = pageController()
     const [hover, sethover] = useState(false)
     const underline = useSpring({
         to: { width: hover ? '100%' : selected==children? '100%': '0%', opacity: hover ? 1 : selected==children? 1: 0 }
     }) 
-    function navigate(){ 
-        window.location.href = `/${slug}`;
+    function navigates(){ 
+        navigate(`/${slug}`)
     }
     useEffect(() => {
         sethover(children === hovered);
     }, [hovered, children]);  
     return (
-        <div onClick={()=>{navigate()}} onMouseEnter={() => sethovered(children)} onMouseLeave={() => sethovered('')} className='h-full flex flex-col justify-center cursor-pointer xl:text-[130%] lg:text-[115%] md:text-[100%]'>
+        <div onClick={()=>{navigates()}} onMouseEnter={() => sethovered(children)} onMouseLeave={() => sethovered('')} className='h-full flex flex-col justify-center cursor-pointer xl:text-[130%] lg:text-[115%] md:text-[100%]'>
             <h1>{children}</h1>
             <div className='w-full flex flex-row justify-center'>
                 <animated.div style={underline} className='w-full border'></animated.div>
